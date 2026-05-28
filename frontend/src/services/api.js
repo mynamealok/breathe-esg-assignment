c9968168e0482c654fc1312f8a451c60d8184b16
Alok Upadhyay <aloku9794@gmail.com>
2026-05-28 22:57:06 +0530

    updated frontenf api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({
-    baseURL: "http://127.0.0.1:8000/api/"
+export default axios.create({
+    baseURL: "https://breathe-esg-assignment-production-5f11.up.railway.app/api/"
 });
-
-export default api;
